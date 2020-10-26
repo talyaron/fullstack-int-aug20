443f528325e09ae077818be25bfe9350521d0f2c
OlgaPan-2020 <72758446+OlgaPan-2020@users.noreply.github.com>
2020-10-26 19:37:48 +0200

    js

---
+++ Javascript/shneur/OlgaPan/script.js
@@ -0,0 +1,11 @@
+let giraffe = {name: "Max", height:'4m', color: 'brown',src:'https://upload.wikimedia.org/wikipedia/commons/c/c7/Rothschild%27s_Giraffe_%28Giraffa_camelopardalis_rothschildi%29_male_%287068054987%29%2C_crop_%26_edit.jpg'}
+
+console.log(giraffe.name)
+console.log(giraffe.height)
+console.log(giraffe.color)
+console.log(giraffe.src)
+
+console.log(giraffe['name'])
+console.log(giraffe['height'])
+console.log(giraffe['color'])
+console.log(giraffe['src'])
