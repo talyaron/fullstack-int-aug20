f5d2f8ddf53d5c2b77ee7664bce60e2058a6a066
Morde Yoiris <morde1515@gmail.com>
2020-10-26 19:07:06 +0200

    Morde added his JS work

---
+++ Javascript/Morde/index.js
@@ -0,0 +1,13 @@
+let favoriteAnimal = {
+    name: "Sloth",
+    size: 1.2 + "cm",
+    src: "https://img.mako.co.il/2018/08/28/shutterstock_243273610_i.jpg",
+    color: "Brown",
+    weight: 20 + "kg"
+}
+
+console.log(favoriteAnimal.name);
+console.log(favoriteAnimal.size);
+console.log(favoriteAnimal.src);
+console.log(favoriteAnimal.color);
+console.log(favoriteAnimal.weight);
