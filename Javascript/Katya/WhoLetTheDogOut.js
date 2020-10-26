beafbbd3a0b8ff96a36f2550745e56b9cfc99ced
Katya Rukosuev <katyafox93@gmail.com>
2020-10-26 20:18:55 +0300

    js

---
+++ Javascript/Katya/WhoLetTheDogOut.js
@@ -0,0 +1,3 @@
+let dog = { name: "Dog", size: "66 cm", src: 'https://tailandfur.com/wp-content/uploads/2015/12/15-Biggest-Dogs-in-the-World-4a.jpg', color: "black", weigth: "45–73 kg" }
+
+console.log(dog);
