e1bcfaaed2c4c554ecd9363973364d4663395405
lior biton <bitonle@gmail.com>
2020-10-26 20:27:49 +0200

    new

---
+++ Javascript/basic/lior/dist/familey.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var familey = ['anat mom', 'age 69', 'lior me', 'age 39', 'moran wife', 'age 38'];
+console.log(familey[0]);
+console.log(familey[1]);
+console.log(familey[2]);
+console.log(familey[3]);
+console.log(familey[4]);
+console.log(familey[5]);
