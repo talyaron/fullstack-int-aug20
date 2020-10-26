38e9f25c8dda663f5c13adff8a33ff242e85418d
Tal <tal.yaron@gmail.com>
2020-10-26 19:16:10 +0300

    javascript init

---
+++ Javascript/basic/dist/varialbels.dev.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var city = 'Kedumim'; //it is an old declaration, similar to let
+
+var name = "Tal"; //string
+
+var family = "Yaron";
+var age = 49; //number;
+
+console.log(name);
+console.log(family);
+console.log(age); //let is a changable var
+
+name = 23;
+console.log(name);
+console.log(family);
+console.log(age);
---
+++ Javascript/basic/varialbels.js
@@ -0,0 +1,16 @@
+var city = 'Kedumim' //it is an old declaration, similar to let
+let name = "Tal"; //string
+const family = "Yaron";
+let age = 49; //number;
+
+console.log(name);
+console.log(family)
+console.log(age)
+
+//let is a changable var
+name = 23;
+
+
+console.log(name);
+console.log(family)
+console.log(age)
