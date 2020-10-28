0fe8540898bb48df5d87f81cbde5401057405676
reem <reemshapiro@gmail.com>
2020-10-28 08:55:32 +0200

    finish

--- Javascript/basic/reem/homework/reem.js
+++ Javascript/basic/reem/homework/reem.js
@@ -10,16 +10,33 @@ console.log(age);
 age=50;
 console.log(age);
 
+// let vehicle=MyCar
+let brend='citroen'
+let speed=180
+let price=20000
+let year=2007
+let color="white"
+
+// console.log(vehicle)
+console.log(brend)
+console.log(speed)
+console.log(price)
+console.log(year)
+console.log(color)
+
+
+const cars =[ ]
 let vehicle={brend:'citroen',speed:180,price:20000,year:2007,color:"white",};
-console.log(vehicle)
+ cars[0 ]=vehicle
+// console.log(cars)
+
+ 
+ vehicle={brend:'tesla',speed:300,price:200000,year:2021,color:"black",};
 
 
-let vehicle1={brend:'tesla',speed:300,price:200000,year:2021,color:"black",};
+cars[1]=vehicle
 
-
-car=[vehicle,vehicle1]
-
-console.log(car)
+console.log(cars)
 
 
 
