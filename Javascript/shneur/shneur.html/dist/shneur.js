899b3528d144fc78a17820a91f5b934f0d9ab895
shneurGreenberg <770abhtur1@gmail.com>
2020-10-29 12:45:40 +0300

    7

--- Javascript/shneur/shneur.html/dist/shneur.js
+++ Javascript/shneur/shneur.html/dist/shneur.js
@@ -1,7 +1,7 @@
 const cars = []
 let vehicle = { brend: 'tesla', model: "rodster", range: 1000, speed: 402, price: 250000, 0 to100: 1.9, color: "red", };
 cars[0] = vehicle
-    // console.log(cars)
+
 
 
 vehicle = { brend: 'tesla', model: "model 3", range: 570, speed: 260, price: 35000, 0 to100: 3.1, color: "white", };
