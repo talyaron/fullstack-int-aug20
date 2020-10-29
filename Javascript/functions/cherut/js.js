e745070664dcbc034d4034971d00af2a0e6b117a
gilad <giladbeer90@gmail.com>
2020-10-29 20:30:52 +0200

    Merge remote-tracking branch 'origin/master'

--- Javascript/functions/cherut/js.js
+++ Javascript/functions/cherut/js.js
@@ -2,5 +2,15 @@ function  CalculateAge(BirthYear, CurrentYear){
   return CurrentYear-BirthYear;
 }
 
-let MyAge = CalculateAge(1998,2020);
-console.log(MyAge);
+// let MyAge = CalculateAge(1998,2020);
+// console.log(MyAge);
+
+
+
+function  NamesToObj(FirstName, surname){
+  let obj = {FirstName:FirstName, surname:surname}
+  console.log(obj)
+  // return obj;
+}
+
+NamesToObj('cherut', 'gur');
---
+++ Javascript/functions/eyal/names/index.js
@@ -0,0 +1,12 @@
+function GetFullName(surName,firstName){
+    const fullName = {firstName:firstName ,surName:surName}
+    fullName.firstName = firstName;
+    fullName.surName = surName;
+    return  fullName;
+   }
+   
+   
+  
+   console.log(GetFullName("eyal","shemuel"))
+
+   /* test */
