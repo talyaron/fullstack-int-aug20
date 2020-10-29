2a007b7f898532e85254b974e41f572d42f33008
eyal shemuel <eshemuel1@gmail.com>
2020-10-29 19:50:04 +0200

    1

---
+++ Javascript/basic/js/js.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function multi(a1, a2) {
+  return a1 * a2;
+}
+
+var a1 = prompt("Please enter your first num", "1");
+var a2 = prompt("Please enter your second num", "2 ");
+console.log(multi(a1, a2));
