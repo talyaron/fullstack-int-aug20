18845dac045d1544db7703a00de542051bdb18bf
eyal shemuel <eshemuel1@gmail.com>
2020-10-29 20:04:44 +0200

    1

---
+++ Javascript/functions/eyal/index.js
@@ -0,0 +1,8 @@
+function age(correntYear,BerthOfYear){
+ return  age =  correntYear - BerthOfYear;
+}
+
+
+var correntYear = prompt("Please enter The Corrent Year ", "1");
+var BerthOfYear = prompt("Please enter your Year of Berth ","2 ");
+console.log(age(correntYear,BerthOfYear))
