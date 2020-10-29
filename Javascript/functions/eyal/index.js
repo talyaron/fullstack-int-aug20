02067a527fe515c571d1e16a3a697983c00e9410
cherut gur <cherut.gur@gmail.com>
2020-10-29 20:47:54 +0200

    g

--- Javascript/functions/eyal/index.js
+++
@@ -1,9 +0,0 @@
-function age(correntYear,BerthOfYear){
- return   correntYear - BerthOfYear;
-}
-
-
-var correntYear = new Date().getFullYear;
-var BerthOfYear = prompt("Please enter your Year of Berth ","1997");
-prompt("your Year of Berth is:",age(correntYear,BerthOfYear));
-console.log(age(correntYear,BerthOfYear))
