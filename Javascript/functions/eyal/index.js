f376878fae67d4513e83d382db30c497a6888d8a
Tal <tal.yaron@gmail.com>
2020-10-29 20:14:57 +0200

    cont

--- Javascript/functions/eyal/index.js
+++ Javascript/functions/eyal/index.js
@@ -3,12 +3,7 @@ function age(correntYear,BerthOfYear){
 }
 
 
-<<<<<<< HEAD
-var correntYear = prompt("Please enter The Corrent Year ", "1");
-var BerthOfYear = prompt("Please enter your Year of Berth ","2");
-=======
 var correntYear = prompt("Please enter The Corrent Year ", "2020");
 var BerthOfYear = prompt("Please enter your Year of Berth ","1997");
 prompt("your Year of Berth is:",age(correntYear,BerthOfYear));
->>>>>>> master
 console.log(age(correntYear,BerthOfYear))
