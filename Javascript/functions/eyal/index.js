be8b3bd465f1d3b22023c8704848ab630c679ed0
Tal <tal.yaron@gmail.com>
2020-10-29 20:09:30 +0200

    cont

--- Javascript/functions/eyal/index.js
+++ Javascript/functions/eyal/index.js
@@ -1,8 +1,8 @@
 function age(correntYear,BerthOfYear){
- return  age =  correntYear - BerthOfYear;
+ return correntYear - BerthOfYear;
 }
 
 
 var correntYear = prompt("Please enter The Corrent Year ", "1");
-var BerthOfYear = prompt("Please enter your Year of Berth ","2 ");
+var BerthOfYear = prompt("Please enter your Year of Berth ","2");
 console.log(age(correntYear,BerthOfYear))
