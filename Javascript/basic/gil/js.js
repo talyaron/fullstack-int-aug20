eff0901ee7b517254a15d47ca9518a87511f1025
Gili <gil.isaacc@gmail.com>
2020-10-29 15:04:36 +0200

    gil

---
+++ Javascript/basic/gil/js.js
@@ -0,0 +1,44 @@
+/* first car */
+
+let brand = "Audi Q2"
+let year = 2017
+let speed ="160hp"
+let color= "white"
+let price= "140k"
+
+console.log(brand,year,speed,color,price)
+
+let car1 = {
+    brand: "Audi Q2",
+    year: 2017,
+    speed: "150hp",
+    color: "white",
+    price: "140k"
+}
+console.log(car1);
+
+const cars=[mycar];
+console.log(cars);
+
+car2 = "Audi Q2"
+year = 2018
+speed = "150hp"
+color = "white"
+price = "170k"
+console.log(car2,year,speed,color,price)
+ 
+car2 = {
+    car2:"Audi Q2",
+    year: 2018,
+    speed: "160hp" ,
+    color: "white" ,
+    price: "170k"
+}
+
+cars=[newcar]
+console.log(cars)
+
+
+
+
+
