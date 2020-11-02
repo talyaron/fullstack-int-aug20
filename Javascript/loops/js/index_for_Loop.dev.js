a4b60d5d449561d4c238a05480e2d9a300e8a1ac
eyal shemuel <eshemuel1@gmail.com>
2020-11-02 18:33:04 +0200

    1

--- Javascript/loops/js/index_for_Loop.dev.js
+++ Javascript/loops/js/index_for_Loop.dev.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /* function countNums(countUntil){
     for (let i=1; i<countUntil;i++){
         console.log(i);
@@ -7,14 +9,15 @@
 countNums(1000);*/
 debugger;
 var movies = [];
+
 function Loopmovies(movie) {
-    for (let i = 1; i < 4; i++) {
-        prompt("enter movie", movie);
-        movies.push(movie);
-        movies.forEach(element => {
-            console.log(element);
-        })
-    }
+  for (var i = 1; i < 4; i++) {
+    prompt("enter movie", movie);
+    movies.push(movie);
+    movies.forEach(function (element) {
+      console.log(element);
+    });
+  }
 }
 
 Loopmovies();
