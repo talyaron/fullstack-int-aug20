2700bd2fb236febccdb3dba3c3755d9b48e7cb5c
eyal shemuel <eshemuel1@gmail.com>
2020-11-02 20:02:51 +0200

    1

--- Javascript/loops/eyal/index_for_Loop.js
+++ Javascript/loops/eyal/index_for_Loop.js
@@ -7,19 +7,19 @@
 countNums(1000);*/
 debugger;
 var movies = [];
+
 const textBo1 = document.getElementById(textbox);
 const DOM1 = document.getElementById(uotput);
 
 function GetMovies() {
-
     movies.push(textBo1.innerText);
-
-}
+};
 
 
 function pintAllMovise(movies) {
+
     movies.forEach(element => {
         DOM1.innerText = `<p> ${element} <p>`
-    })
+    });
 
-}
+};
