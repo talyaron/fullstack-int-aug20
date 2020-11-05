1cb94039d4a4e8e448855c2fcf7ef8e1562729f7
OlgaPan-2020 <72758446+OlgaPan-2020@users.noreply.github.com>
2020-11-05 19:20:38 +0200

    eventOlga

---
+++ Javascript/events/Olga/event.js
@@ -0,0 +1,8 @@
+const theDiv = document.getElementById('theDiv')
+function handleClick(event) {
+    event.target.style.transform = 'translate(20px, 0)'
+}
+
+function handleClick2(event) {
+    event.target.style.transform = 'translate(-20px, 0)'
+}
