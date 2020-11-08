19efa6b3c4f365f543b054304e59f1883a78466d
eyal shemuel <eshemuel1@gmail.com>
2020-11-08 14:39:00 +0200

    V2

---
+++ Javascript/events/eyal/homework/version2/js/index.js
@@ -0,0 +1,64 @@
+
+        const Cloun = document.querySelector('#Cloun');
+        const TopCover = document.querySelector('#topCover');
+        const box = document.querySelector('#box');
+        const boxButton = document.querySelector('.box');
+        const plane = document.querySelector('#plane');
+        const planeButton = document.querySelector('.plane');
+
+        /* ליצן מופיע בלחיצה על קופסא */
+        box.addEventListener('click', function (event1) {//box clicked
+            console.log("box clicked");
+            Cloun.style.width = "100px";
+            Cloun.style.height = "100px";
+            Cloun.style.transform = 'translate(0px,-100px)';
+            Cloun.src = "img/clown.png";
+            TopCover.style.top = "460px";
+            TopCover.style.transform = "skewY(-20deg)";
+            /*   top: 460px;
+          transform: skewY(-20deg); */
+
+            box.addEventListener('mouseleave', function (event) {
+                console.log("Cloun clicked");
+                Cloun.style.transform = 'translate(0px,20px)';
+                /*  Cloun.src = ""; */
+                /* Cloun.style.width = "0px";
+                Cloun.style.height = "0px"; */
+                TopCover.style.top = "495px";
+                TopCover.style.transform = "skewY(0deg)";
+            });
+
+        });
+
+
+        /* ליצן מופיע בלחיצה על מטוס */
+        plane.addEventListener('click', function (event1) {//plane clicked
+            console.log("box clicked");
+            Cloun.style.width = "100px";
+            Cloun.style.height = "100px";
+            Cloun.style.transform = 'translate(0px,-100px)';
+            Cloun.src = "img/clown.png";
+            TopCover.style.top = "460px";
+            TopCover.style.transform = "skewY(-20deg)";
+
+
+            plane.addEventListener('mouseleave', function (event) {
+                console.log("Cloun clicked");
+                Cloun.style.transform = 'translate(0px,20px)';
+            });
+
+        });
+
+
+        boxButton.addEventListener('click', function (event1) {
+            plane.style.display = "none";
+            TopCover.style.display = "block";
+            box.style.display = "block";
+
+        });
+
+        planeButton.addEventListener('click', function (event1) {
+            plane.style.display = "block";
+            TopCover.style.display = "none";
+            box.style.display = "none";
+        });
