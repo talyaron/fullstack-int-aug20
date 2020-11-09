5b59604ab5c8d06602a025d600fa581920774d80
eyal shemuel <eshemuel1@gmail.com>
2020-11-09 10:28:24 +0200

    version 2 finished

--- Javascript/events/eyal/homework/version2/js/index.js
+++ Javascript/events/eyal/homework/version2/js/index.js
@@ -1,84 +1,90 @@
 
-        const Cloun = document.querySelector('#Cloun');
-        const TopCover = document.querySelector('#topCover');
-        const box = document.querySelector('#box');
-        const boxButton = document.querySelector('.box');
-        const plane = document.querySelector('#plane');
-        const planeButton = document.querySelector('.plane');
+const Cloun = document.querySelector('#Cloun');
+const TopCover = document.querySelector('#topCover');
+const box = document.querySelector('#box');
+const boxButton = document.querySelector('.box');
+const plane = document.querySelector('#plane');
+const planeButton = document.querySelector('.plane');
 
-        /* ליצן מופיע בלחיצה על קופסא */
-        box.addEventListener('click', function (event1) {//box clicked
-            console.log("box clicked");
-            Cloun.style.width = "100px";
-            Cloun.style.height = "100px";
-            Cloun.style.transform = 'translate(0px,-100px)';
-            Cloun.src = "img/clown.png";
-            TopCover.style.top = "460px";
-            TopCover.style.transform = "skewY(-20deg)";
-            /*   top: 460px;
-          transform: skewY(-20deg); */
-
-            box.addEventListener('mouseleave', function (event) {
-                console.log("Box leaved");
-                Cloun.style.transform = 'translate(0px,20px)';
-                /*  Cloun.src = ""; */
-                /* Cloun.style.width = "0px";
-                Cloun.style.height = "0px"; */
-                TopCover.style.top = "495px";
-                TopCover.style.transform = "skewY(0deg)";
-            });
-
-        });
+/* ליצן מופיע בלחיצה על קופסא */
+box.addEventListener('click', function (event1) {//box clicked
+    console.log("box clicked");
+    Cloun.style.width = "100px";
+    Cloun.style.height = "100px";
+    Cloun.style.transform = 'translate(0px,-100px)';
+    Cloun.src = "img/clown.png";
+    TopCover.style.top = "460px";
+    TopCover.style.transform = "skewY(-20deg)";
 
 
-        /* ליצן מופיע בלחיצה על מטוס */
-        plane.addEventListener('click', function (event1) {//plane clicked
-            console.log("box clicked");
-            Cloun.style.width = "100px";
-            Cloun.style.height = "100px";
-            Cloun.style.transform = 'translate(0px,-100px)';
-            Cloun.src = "img/clown.png";
-            TopCover.style.top = "460px";
-            TopCover.style.transform = "skewY(-20deg)";
+    box.addEventListener('mouseleave', function (event) {//Box leaved
+        console.log("Box leaved");
+        Cloun.style.transform = 'translate(0px,20px)';
+        TopCover.style.top = "495px";
+        TopCover.style.transform = "skewY(0deg)";
+    });
+
+});
 
 
-            plane.addEventListener('mouseleave', function (event) {
-                console.log("Plane leaved");
-                Cloun.style.transform = 'translate(0px,20px)';
-            });
+/* ליצן מופיע בלחיצה על מטוס */
+plane.addEventListener('click', function (event1) {//plane clicked
+    console.log("box clicked");
+    Cloun.style.width = "100px";
+    Cloun.style.height = "100px";
+    Cloun.style.transform = 'translate(0px,-100px)';
+    Cloun.src = "img/clown.png";
+    TopCover.style.top = "460px";
+    TopCover.style.transform = "skewY(-20deg)";
 
-        });
+
+    plane.addEventListener('mouseleave', function (event) {//Plane leaved
+        console.log("Plane leaved");
+        Cloun.style.transform = 'translate(0px,20px)';
+    });
+
+});
+
+/*   לחצנים לשינוי התצוגה */
+boxButton.addEventListener('click', function (event1) {
+    plane.style.display = "none";
+    TopCover.style.display = "block";
+    box.style.display = "block";
+
+});
+
+planeButton.addEventListener('click', function (event) {
+    plane.style.display = "block";
+    TopCover.style.display = "none";
+    box.style.display = "none";
+});
 
 
-        boxButton.addEventListener('click', function (event1) {
-            plane.style.display = "none";
-            TopCover.style.display = "block";
-            box.style.display = "block";
 
-        });
+/*   גרירת המטוס */
+let deltaX
+let deltaY
 
-        planeButton.addEventListener('click', function (event1) {
-            plane.style.display = "block";
-            TopCover.style.display = "none";
-            box.style.display = "none";
-        });
-        plane.addEventListener('dragstart', function dragStart(event2){
-            
-            setTimeout(() => (this.className = 'invisible'), 0); 
-            var position = event2.target;
-            console.log( position.x)
-            plane.style.left = position.x + 'px';
-            plane.style.top = position.y + 'px';
-            console.log( plane.style.left)
-        });
+plane.addEventListener('dragstart', event => {
+    setTimeout(() => (this.className = 'invisible'), 0);
+    var position = event.target;
+    x = position.offsetLeft;
+    y = position.offsetTop;
+    deltaX = event.x - x;
+    deltaY = event.y - y;
+    console.log(x, y);
 
-        plane.addEventListener('dragend', function dragStart(event2){
-            
-            this.className = 'visible';
-              var position = event2.target;
-              console.log( position.x)
-              plane.style.left = position.x + 'px';
-              plane.style.top = position.y + 'px';
-              console.log( plane.style.left)
-          });
-  
+});
+
+
+plane.addEventListener('dragend', function dragStart(event) {
+
+    this.className = 'visible';
+    var position = event.target;
+    plane.style.left = (event.x - (deltaX)) + 'px';
+    plane.style.top = (event.y - (deltaY)) + 'px';
+    Cloun.style.left = (event.x +  - (deltaX/2)) + 'px';
+    Cloun.style.top = (event.y - (deltaY/2)) + 'px';
+    console.log(event)
+});
+
