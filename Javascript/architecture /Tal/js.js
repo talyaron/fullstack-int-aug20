3a26995d8e4b839df6ada58cd33bbe0980381048
Tal <tal.yaron@gmail.com>
2020-11-12 19:37:33 +0200

    whatsapp

---
+++ Javascript/architecture /Tal/js.js
@@ -0,0 +1,28 @@
+// const messages = []
+const messagesWrapper = document.querySelector('#messagesWrapper');
+
+function handleSubmitMessage(e) {
+    e.preventDefault()
+    console.log(e)
+    const message = e.target.elements.message.value
+    console.log(message)
+
+    // messages.push(message);
+    if (message.length > 0) {
+        document.querySelector('#messagesWrapper').innerHTML += htmlMessage(message);
+        e.target.reset();
+        messagesWrapper.scrollTo(0,messagesWrapper.scrollHeight);
+    }
+}
+
+function htmlMessage(message) {
+    return `<div class='message'>${message}</div>`
+}
+
+function handleTextArea(e){
+   
+    if(e.key === 'Enter' && e.shiftKey == false){
+        //submit
+        document.getElementById('submitButton').click();
+    }
+}
