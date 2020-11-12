124502daa34ef3f2ac16c12bf875dc7bce47feb3
eyal shemuel <eshemuel1@gmail.com>
2020-11-12 20:39:52 +0200

    1

---
+++ Javascript/architetctur/js/js.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var Box = document.querySelectorall(".Box");
+var BoxWrapperOUTput = document.querySelectorall("#BoxWrapperOUTput");
+
+function creatBox() {
+  BoxWrapperOUTput.innethtml += "<div class=\"box\"></div>";
+}
+
+box.addEventListener('click', function (event) {
+  creatBox();
+});
---
+++ Javascript/architetctur/room6/js.js
@@ -0,0 +1,11 @@
+const Box = document.querySelectorall(".Box")
+const BoxWrapperOUTput = document.querySelectorall("#BoxWrapperOUTput")
+
+function creatBox(){
+    BoxWrapperOUTput.innethtml += `<div class="box"></div>`;
+}
+
+
+box.addEventListener('click',function (event) {
+    creatBox()
+})
