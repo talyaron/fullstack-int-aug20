b75a4eda4371c1400e751b21224eb311da2e1f45
reem <reemshapiro@gmail.com>
2020-11-19 19:17:32 +0200

    cont

---
+++ Javascript/projects/zap/reemProduct.js
@@ -0,0 +1,33 @@
+const products=[
+{
+    category:'computers',
+    price:968,
+    inStoreSince:'2020-01-23',
+    brand:'Asus',
+    model:'  L203NA-FD126TS',
+    recommendations:138,
+    image:'https://www.bestmobile.co.il/media/catalog/product/2/0/2031.png',
+    description:"achala computer",
+    name:'11.6" L203NA-FD126TS 4G',
+    store:'BestMobil',
+    isdn:'drytertyertyerh'
+},
+
+{
+    category:'computers',
+    price:2789,
+    inStoreSince:'2020-04-15',
+    brand:'Lenovo',
+    model:' Lenovo V14 IIL 82C400SDIV',
+    recommendations:38,
+    image:'https://img.zap.co.il/pics/5/1/9/1/58201915c.gif',
+    description:"achala computer",
+    name:'11.6" L203NA-FD126TS 4G',
+    store:'Pc-Online',
+    isdn:'drytertyertyerh'
+},]
+
+
+
+
+]
