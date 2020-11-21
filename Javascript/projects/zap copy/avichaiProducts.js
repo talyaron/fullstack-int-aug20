12513e02716e8c8d1900990a566f707f95ef963d
Avichai Nissim <avichai74@gmail.com>
2020-11-21 21:29:10 +0200

    1

---
+++ Javascript/projects/zap copy/avichaiProducts.js
@@ -0,0 +1,41 @@
+const products=[
+    {
+        category:'mobile',
+        price:5500,
+        inStoreSince:2020-09-22,
+        brand:'Iphone',
+        model:'iPhone 12 Pro',
+        recommendations:85,
+        image:'https://www.gizmochina.com/wp-content/uploads/2020/05/iphone-12-pro-max-family-hero-all-600x600.jpg',
+        description:"very high end mobile phone",
+        name:'iPhone 12 Pro',
+        store:'Apple',
+        isdn:'SNJ28DN1900SMC89S82'
+    },
+    {
+        category:'fashion',
+        price:2000,
+        inStoreSince:'2020-04-05',
+        brand:'GUCCI',
+        model:'Ace Sneakers',
+        recommendations:85,
+        image:'https://cache.net-a-porter.com/images/products/1009127/1009127_in_920_q80.jpg',
+        description:"nice shoes",
+        name:'GUCCI Ace Sneakers',
+        store:'net a porter',
+        isdn:'ASMJDKK29CM10DCM22'
+    },
+    {
+        category:'electricity',
+        price:850,
+        inStoreSince:2019-02-22,
+        brand:'Electra',
+        model:'ELECTRA 5100',
+        recommendations:92,
+        image:'https://www.payngo.co.il/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/5/6/5600-1_1.jpg',
+        description:"good washer",
+        name:'Electra ELECTRA 5100',
+        store:'electra',
+        isdn:'SDMKMKS929321CSSDQSDZXX'
+    },
+]
