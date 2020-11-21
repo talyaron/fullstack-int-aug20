8eb10b847e2d6e0067402032a8f81c9789e848e1
cherut gur <cherut.gur@gmail.com>
2020-11-22 01:30:51 +0200

    f

--- Javascript/projects/Avichai Reem Hilel Cherut/createCategories.js
+++ Javascript/projects/Avichai Reem Hilel Cherut/createCategories.js
@@ -1,5 +1,3 @@
-
-
 const categoryBox = document.querySelector("#categoryBox")
 let filter = []
 
@@ -20,40 +18,56 @@ let category = document.querySelectorAll('.category')
 let test = document.querySelector("#test")
 let html1 = ""
 let productName = ""
+let filterButtons = document.querySelector("#filterButtons")
+
+let currentCategory = [];
 
 function handleClick(e) {
     productName = e.target.value
 
     test.innerHTML = ""
-    for (i = 0; i < products.length; i++) {
-        if (productName == products[i].category) {
-            test.innerHTML += `<p>Product name: ${products[i].category}, Product Model: ${products[i].model}, Price: ${products[i].price}</p>`
+    products.forEach(product =>{
+        if(productName == product.category){
+            console.log (product.category)
+            currentCategory.push(product);
+            test.innerHTML += `<p>Product Category: ${product.category}, Product Model: ${product.model}, Price: ${product.price}, Recommendations: ${product.recommendations}</p>`
+            filterButtons.innerHTML += `<button onclick="handleFilter(event)">${product.brand}</button>`;
+        }  
+    })
+
+    function handleFilter(event){
+        console.log('handleFilter(event)')
+    }
+
+    // for (i = 0; i < products.length; i++) {
+    //     if (productName == products[i].category) {
+    //         test.innerHTML += `<p>Product name: ${products[i].category}, Product Model: ${products[i].model}, Price: ${products[i].price}, Recommendations: ${products[i].recommendations}</p>`
             
-        }
-    }
+    //     }
+    // }
 }
 
 
-function handlePrice(e) {
-    products.sort((a, b) => a.price - b.price)
-    test.innerHTML = ""
-    for (i = 0; i < products.length; i++) {
-        if (productName == products[i].category) {
-            test.innerHTML += `<p>Product name: ${products[i].category}, Product Model: ${products[i].model}, Price: ${products[i].price}</p>`
+// function handlePrice(e) {
+//     products.sort((a, b) => a.price - b.price)
+//     test.innerHTML = ""
+//     for (i = 0; i < products.length; i++) {
+//         if (productName == products[i].category) {
+//             test.innerHTML += `<p>Product name: ${products[i].category}, Product Model: ${products[i].model}, Price: ${products[i].price}, Recommendations: ${products[i].recommendations}</p>`
 
-        }
-    }
-}
+//         }
+//     }
+// }
 
-function handleRecommend(e) {
-    products.sort((a, b) => a.recommendations - b.recommendations)
-    test.innerHTML = ""
-    for (i = 0; i < products.length; i++) {
-        if (productName == products[i].category) {
-            test.innerHTML += `<p>Product name: ${products[i].category}, Product Model: ${products[i].model}, Price: ${products[i].price}</p>`
-        }
-    }
-}
+// function handleRecommend(e) {
+//     products.sort((a, b) => a.recommendations - b.recommendations)
+//     test.innerHTML = ""
+//     for (i = 0; i < products.length; i++) {
+//         if (productName == products[i].category) {
+//             test.innerHTML += `<p>Product name: ${products[i].category}, Product Model: ${products[i].model}, Price: ${products[i].price}, Recommendations: ${products[i].recommendations}</p>`
+//         }
+//     }
+// }
 
 // filter.forEach(product => {
 //     if (filter.indexOf(product.category) == -1) {
