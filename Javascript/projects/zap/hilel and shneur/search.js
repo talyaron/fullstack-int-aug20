005c987551b839d538e51568f345690c9db08ad4
hillel <a4105962@gmail.com>
2020-11-22 16:19:37 +0200

    1

--- Javascript/projects/zap/hilel and shneur/search.js
+++ Javascript/projects/zap/hilel and shneur/search.js
@@ -1,17 +1,27 @@
+// מערך אוטומטי שנוצר + קטגוריה
+let ProductsFiltered = []
+
+// דוחף נתונים למערך ללא כפולים
+products.forEach(product => {
+    if (ProductsFiltered.indexOf(product.category) == -1) {
+        ProductsFiltered.push(product.category)
+    }
+})
+
+
+// חיפוש + קריאה לפונקציות התוצאה
 function handleSearch(e) {
     e.preventDefault();
-
     const searchTerm = e.target.children.search.value;
-
     if (searchTerm.length > 2) {
         const results = searchProducts(searchTerm);
         renderSearchResults(results);
-
     }
 }
-let results = []
 
+// תנאי החיפוש
 function searchProducts(searchTerm) {
+    let results = []
     const regSearch = new RegExp(searchTerm, 'g')
     products.forEach(product => {
         if (regSearch.test(product.name) || regSearch.test(product.category) || regSearch.test(product.model)) {
@@ -19,14 +29,12 @@ function searchProducts(searchTerm) {
             hil();
         }
     })
-
-
     return results;
 }
 
+// מעביר תוצאות ל html
 function renderSearchResults(results) {
     const root = document.getElementById('root');
-
     let html = '';
     results.forEach(product => {
         html += `<a href="${product.Link}"><div class="card"><p>Product name:${product.name}</br> Product Model: ${product.model}</br> Price: ${product.price}</p></div></a>`
@@ -34,30 +42,50 @@ function renderSearchResults(results) {
     root.innerHTML = html;
 }
 
+// הצגת קטוגוריות
+let textCategory = "";
+function showCategory(event) {
+    for (i = 0; i < ProductsFiltered.length; i++) {
+        textCategory += `<div class="category"value="${ProductsFiltered[i]}" onclick="openCtegory(event)">${ProductsFiltered[i]}</div>`
+        document.querySelector("#category").innerHTML = textCategory;
+    }
+
+}
+function openCtegory(event) {
+    const prodctfilter = event.target.innerText
+    console.log(prodctfilter)
+    console.log(event)
+    products.forEach(function (event) {
+        console.log(event.category)
+        // html+= `<div onclick="${}"></div>`
+    })
+}
+// console.log(products.name)
+
 const core = document.querySelector(".core")
 const button = document.querySelector("#button")
 const cotert = document.querySelector(".cotert")
 const root = document.querySelector("#root")
-
+const div = document.querySelector(".div")
 
 function hil() {
     core.style.display = "block"
-    cotert.style.borderRadius = "10px 10px 0 0"
-    root.style.border = '5px solid rgb(105, 105, 105)'
+    cotert.style.borderRadius = "10px 10px 0px 0px"
+    root.style.border = '15px solid rgba(105, 105, 105, 0.102)'
 
 }
 
 let roothtml = ""
+
 function hiderezolt(event) {
     const input = event.target.value;
-    console.log(event)
-    if (input.length < 1 || input.length > 5) {
-
-        root. innerHTML = roothtml
-
+    if (input.length < 1) {
+        root.innerHTML = roothtml
+        core.style.display = "none"
+        cotert.style.borderRadius = "10px 10px 0px 0px"
+        root.style.border = '0px solid  rgba(255, 255, 255, 0)'
     }
-    else {
-        console.log('hillel')
-    }
-    // root.innerHTML = roothtml
 }
+
+
+
