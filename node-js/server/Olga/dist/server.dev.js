690f3765dbc262f81076e153d7cc144fdbc260c1
OlgaPan-2020 <72758446+OlgaPan-2020@users.noreply.github.com>
2020-12-02 23:14:26 +0200

    1

--- node-js/server/Olga/dist/server.dev.js
+++ node-js/server/Olga/dist/server.dev.js
@@ -2,13 +2,52 @@
 
 var http = require('http');
 
-var fs = require('fs'); //create a server object:
+var fs = require('fs');
 
+var count = 1; //create a server object:
 
 http.createServer(function (req, res) {
-  fs.readFile('index.html', 'utf-8', function (err, data) {
-    res.write(data); //write a response to the client
+  console.log("".concat(count++, ") ").concat(req.url));
 
-    res.end(); //end the response
-  });
-}).listen(3000); //the server object listens on port 3000
+  if (req.url === '/') {
+    console.log('1......');
+    fs.readFile('index.html', 'utf-8', function (err, data) {
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      res.write(data); //write a response to the client
+
+      res.end(); //end the response
+    });
+  } else if (req.url === '/style.css') {
+    console.log('2......');
+    fs.readFile('style.css', 'utf-8', function (err, data) {
+      res.writeHead(200, {
+        'Content-Type': 'text/css'
+      });
+      res.write(data); //write a response to the client
+
+      res.end(); //end the response
+    });
+  } else if (req.url === '/function.js') {
+    fs.readFile('function.js', 'utf-8', function (err, data) {
+      res.writeHead(200, {
+        'Content-Type': 'text/javascript'
+      });
+      res.write(data); //write a response to the client
+
+      res.end(); //end the response
+    });
+  } else {
+    fs.readFile('404.html', 'utf-8', function (err, data) {
+      res.writeHead(404, {
+        'Content-Type': 'text/html'
+      });
+      res.write(data); //write a response to the client
+
+      res.end(); //end the response
+    });
+  }
+}).listen(3000, function () {
+  console.log('server listen on port 3000');
+});
