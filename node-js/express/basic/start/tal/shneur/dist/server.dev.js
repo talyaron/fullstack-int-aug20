ca5dd486a159ad41c0e88ae7141a2feecfc0c331
shneur <770abhtur1@gmail.com>
2020-12-03 19:45:02 +0200

    gh

--- node-js/express/basic/start/tal/shneur/dist/server.dev.js
+++ node-js/express/basic/start/tal/shneur/dist/server.dev.js
@@ -4,6 +4,7 @@ var express = require('express');
 
 var app = express();
 app.use(express["static"]('public'));
+var timeSinceRefresh;
 var port = 3000;
 app.listen(port, function () {
   console.log("ghhdie uhueb ugdyed ug");
--- node-js/express/basic/start/tal/shneur/server.js
+++ node-js/express/basic/start/tal/shneur/server.js
@@ -3,11 +3,12 @@ const app = express()
 app.use(express.static('public'))
 
 
+let timeSinceRefresh
+
 
 
 const port = 3000
 app.listen(port,() =>{
     console.log("ghhdie uhueb ugdyed ug")
-
 
 })
