1884ad7474f7d22642d361c98f4013b197bf7eea
shneur <770abhtur1@gmail.com>
2020-12-03 19:41:31 +0200

    f

---
+++ node-js/express/basic/start/tal/shneur/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
+
+
+
+
+const port = 3000
+app.listen(port,() =>{
+    console.log("ghhdie uhueb ugdyed ug")
+
+
+})
