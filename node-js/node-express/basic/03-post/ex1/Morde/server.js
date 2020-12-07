0b58bfe406c0ec8fa3ab12ffca313a9d9103e906
Morde Yoiris <morde1515@gmail.com>
2020-12-07 20:34:12 +0200

    morde Added

--- node-js/node-express/basic/03-post/ex1/Morde/server.js
+++ node-js/node-express/basic/03-post/ex1/Morde/server.js
@@ -1,23 +1,24 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
-const usersArray = ["morde","tal","eyal","guy"];
+const usersArray = ["morde", "tal", "eyal", "guy"];
 
-app.post("/login",(req,res)=>{
-    const username = req.body.username;
-    const regExp = new RegExp(username,"g");
-    if(regExp.test(usersArray)){
-        res.redirect("/success.html")
-    }else{
-        res.redirect("/failure.html")
-    }
-})
-
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const regExp = new RegExp(username, "g");
+  if (regExp.test(usersArray)) {
+    res.redirect("/success.html");
+  } else {
+    res.redirect("/failure.html");
+  }
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, ()=>{console.log('server listen on port', port)})
+app.listen(port, () => {
+  console.log("server listen on port", port);
+});
