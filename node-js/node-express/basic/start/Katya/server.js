2420a8516aeda496d2fd55bdbe2d80020f9ec1f6
Katya Rukosuev <katyafox93@gmail.com>
2020-12-07 15:19:24 +0200

    1

--- node-js/node-express/basic/start/Katya/server.js
+++ node-js/node-express/basic/start/Katya/server.js
@@ -3,21 +3,22 @@ const app = express();
 
 let numberOfDogs = 3;
 
+//midleware
+app.use((req, res, next) => {
+    console.log(`someone called the server: ${req.url} and I have ${numberOfDogs} dogs`)
+    next()
+})
 
 app.use(express.static('public')) //ניתן לתת שם לתיקיה שרוצים, נהוג לתת שם פאבליק..צריך לשים בפאבליק בשביל שלא יהיה ניתן להגיע לשרת
     //אם נרשום בlocalhost:3000/secret.html נוכל לראות את הקובץ שיש בתיקיה
 
-//midleware
-app.use((req, res, next) => {
-    console.log('someone called the server', req.url)
-    next()
-})
+
 
 
 
 app.get('/get-number-of-dogs', (req, res) => { //the server response to tthe client
     res.send({ numberOfDogs })
-        // numberOfDogs++
+    numberOfDogs++
 })
 
 
