c43a8835d5bd6a02eb919f94f897e0c9395572b1
Katya Rukosuev <katyafox93@gmail.com>
2020-12-08 16:10:47 +0200

    done

--- node-js/node-express/HW1/KatyaExpreesHW/public/js.js
+++ node-js/node-express/HW1/KatyaExpreesHW/public/js.js
@@ -1,10 +1,9 @@
 // alert('check')
 
-// const { response } = require("express");
 fetch('/get-jokes') //give me 'what wrote here'
     .then(response => response.json()) //after do it to json
     .then(data => { //give the data
-        document.getElementById('root').innerText = `${data.ran}` //inner in root in htnl
+        document.getElementById('root').innerHTML = `<div class="one">${data.ran}</div>` //inner in root in htnl
     })
 
 setInterval(() => {
@@ -12,7 +11,7 @@ setInterval(() => {
     fetch('/get-jokes') //give me 'what wrote here'
         .then(response => response.json()) //after do it to json
         .then(data => { //give the data
-            document.getElementById('root').innerText = `<p class="one"> ${data.ran}</p>` //inner in root in htnl
+            document.getElementById('root').innerHTML = `<div class="one">${data.ran}</div>` //inner in root in htnl
         })
 
-}, 2000)
+}, 7000)
