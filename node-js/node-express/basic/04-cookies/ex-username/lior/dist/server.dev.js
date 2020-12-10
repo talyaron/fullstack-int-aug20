969824c378882610ae56b7c197e5b419603fa0ee
lior biton <bitonle@gmail.com>
2020-12-10 20:57:19 +0200

    1

--- node-js/node-express/basic/04-cookies/ex-username/lior/dist/server.dev.js
+++ node-js/node-express/basic/04-cookies/ex-username/lior/dist/server.dev.js
@@ -6,9 +6,17 @@ var app = express();
 
 var cookieParser = require('cookie-parser');
 
+var bodyParser = require("body-parser");
+
 app.use(cookieParser());
-var _ref = "",
-    username = _ref.username;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+use.post("/getusername"), function (req, res) {
+  var usernameGiven = req.body.username;
+  var cookie = req.cookies.cookie;
+};
 app.use(express["static"]('public'));
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
--- node-js/node-express/basic/04-cookies/ex-username/lior/server.js
+++ node-js/node-express/basic/04-cookies/ex-username/lior/server.js
@@ -3,16 +3,29 @@ const app = express();
 
 const cookieParser = require('cookie-parser');
 
-app.use(cookieParser());
-
-let {username} = ""
-
-app.post('/user', (req, res) => {
-    console.log
-}
-
+const bodyParser = require("body-parser");
 
 app.use(express.static('public'))
 
+app.use(cookieParser());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+use.post("/getusername"), (req,res) =>{
+    const usernameGiven = req.body.username;
+  const { cookie } = req.cookies;
+  if (cookie !== yourename) {
+    res.cookie("yourename", yourename, {
+      maxAge: 200000000,
+      httpOnly: true,
+    });
+}
+
 const port = process.env.PORT || 3000;
-app.listen(port,()=>{console.log(`Listen on port ${port}`)});
+
+app.listen(port, () => {
+  console.log(`listen on port ${port}`);
+});
