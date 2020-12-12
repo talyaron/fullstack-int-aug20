55a057d784c148930f23683880196e958ac02f55
lior biton <bitonle@gmail.com>
2020-12-12 18:39:43 +0200

    lasthw

--- node-js/node-express/basic/03-post/ex1/lior/server.js
+++ node-js/node-express/basic/03-post/ex1/lior/server.js
@@ -1,14 +1,35 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+const bodyParser = require('body-Parser');
+
+app.use(bodyParser.json());
 
 app.use(express.static('public'))
 
-const useres = [{name:'lior',code:'1111'},
-{name:'moran',code:'2222'},
-{name:'gilad',code:'3333'},
-{name:'cherut',code:'4444'},
-{name:'olga',code:'5555'}]
+const login = [{username:'lior',password:'1111'},
+{username:'moran',password:'2222'},
+{username:'cherut',password:'3333'},
+{username:'tal',password:'4444'}]
 
+    const user = login.map(a => a.username);
+    const pass = login.map(a => a.password);
+    
+    app.post('/send_login_information', (req, res) => {
+
+        console.log('sending to the claient: ' + req.body);
+        if ((req.body.UserID == user) && (req.body.password == Pass)) {
+    
+            res.send({
+                ok: true
+            });
+    
+        } else {
+            res.send({
+                ok: false
+            });
+        }
+    
+    })
 
 const port = process.env.PORT || 3000;
-app.listen(port, ()=>{console.log(`listen on port ${port}`)})
+app.listen(port, () => { console.log(`listen on port ${port}`) }) 
