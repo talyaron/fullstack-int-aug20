0a4959be9561fe5ec50c9d4d3c03654bf387fd6c
cherut gur <cherut.gur@gmail.com>
2020-12-12 19:06:17 +0200

    checkUser

--- node-js/node-express/basic/03-post/ex1/cherut/public/js.js
+++ node-js/node-express/basic/03-post/ex1/cherut/public/js.js
@@ -1,21 +1,29 @@
 function handleCheckUser(e){
-  if (e.keyCode === 13) {
-    e.preventDefault();
-    let nameToCheck = e.target.value
+
+    const nameToCheck = e.target.parentElement.children[0].value
+    const passwordToCheck = e.target.parentElement.children[1].value
+    const root = document.querySelector('#root')
     
     fetch('/check-user', {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json'
       },
-      body: JSON.stringify({nameToCheck})
+      body: JSON.stringify({nameToCheck, passwordToCheck})
       }).then(res=>res.json())
       .then(data=>{
-        if(data.k == true){
-          document.body.style.backgroundColor = 'green';
+        if(data.ok == true){
+          document.body.style.backgroundColor = ' rgba(102, 255, 102, 0.705)';
+          root.innerHTML = `<h1>The user "${nameToCheck}" exists on the server</h1>`;
+          // let inputs = document.getElementsByTagName('input');
+          // console.log(inputs)
+          // console.log(e.target)
+          // e.target.value = '';
         }else {
-          document.body.style.backgroundColor = 'red';
+          document.body.style.backgroundColor = ' rgba(255, 121, 121, 0.644)';
+          root.innerHTML = `<h1>The user "${nameToCheck}" is not exists on the server</h1>`;
+          e.target.value = '';
         }
     })
-  }
+  
 }
