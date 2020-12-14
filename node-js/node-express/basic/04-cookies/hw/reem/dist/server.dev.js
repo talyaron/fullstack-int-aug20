40f73ecc06a79711c333af75fdbf11ebd2463933
reem <reemshapiro@gmail.com>
2020-12-14 07:54:57 +0200

    cont

--- node-js/node-express/basic/04-cookies/hw/reem/dist/server.dev.js
+++ node-js/node-express/basic/04-cookies/hw/reem/dist/server.dev.js
@@ -10,17 +10,23 @@ var cookieParser = require('cookie-parser');
 
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 var user = [];
 app.post('/send-user', function (req, res) {
   user.push(req.body.user);
-  console.log(user); // const {user} = req.body;
+  var json_str = JSON.stringify(user); // const {user} = req.body;
 
-  res.cookie('user', user, {
+  res.cookie('user', json_str, {
     maxAge: 5000000,
     httpOnly: true
   });
+  var json_perse = req.cookies.user;
+  var arr = JSON.parse(json_perse);
   res.send({
-    ok: true
+    ok: true,
+    arr: arr
   });
 });
 app.use(express["static"]('public'));
