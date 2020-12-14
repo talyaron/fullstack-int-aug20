f26cae84aa55e3394fa332ff07dadc82dd6f3db1
Tal <tal.yaron@gmail.com>
2020-12-14 19:16:18 +0200

    cherut

--- node-js/node-express/basic/04-cookies/hw/cherut/server.js
+++ node-js/node-express/basic/04-cookies/hw/cherut/server.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 app.use(bodyParser.json())
 app.use(cookieParser());
 
-const user = [];  
+const user = [];
 
 // app.use((req, res, next)=>{
 
@@ -19,24 +19,37 @@ const user = [];
 
 
 
-app.post('/send-user',(req, res)=>{
+app.post('/send-user', (req, res) => {
 
-    // {user:['cherut', 'or']}
-   
-    user.push(req.body.user);
-    let json_str = JSON.stringify(user);
+    try {
 
-    
-    res.cookie('user', json_str, { maxAge: 50000000 });
+        let cookies = req.cookies.user;
+        console.log(cookies)
 
-    let json_perse = req.cookies.user;
-    let arr = JSON.parse(json_perse);
-    res.send({ok:true, arr})
-})
+        
+
+        // // {user:['cherut', 'or']}
+
+
+        let user = req.body.user;
+
+        cookies += ';'+user
+        // cookies.push(json_str);
+        // console.log(cookies);
+
+        res.cookie('user', cookies, { maxAge: 50000000, httpOnly:false});
+
+        // let json_perse = req.cookies.user;
+        // let arr = json_perse;
+        res.send({ ok: true })
+    } catch (e) {
+        console.log("error:", e)
+    } 
+})  
 
 
 
 app.use(express.static('public'))
 
 const port = process.env.PORT || 3000;
-app.listen(port,()=>{console.log(`Listen on port ${port}`)});
+app.listen(port, () => { console.log(`Listen on port ${port}`) });
