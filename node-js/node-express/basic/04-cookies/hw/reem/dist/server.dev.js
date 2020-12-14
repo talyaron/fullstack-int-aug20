3be3aa882d4d32238bbd76021ca51a30b3f5edd6
reem <reemshapiro@gmail.com>
2020-12-14 16:32:49 +0200

    cont

--- node-js/node-express/basic/04-cookies/hw/reem/dist/server.dev.js
+++ node-js/node-express/basic/04-cookies/hw/reem/dist/server.dev.js
@@ -16,17 +16,26 @@ app.post('/send-user', function (req, res) {
   user.push(req.body.user);
   var json_str = JSON.stringify(user); // const {user} = req.body;
 
-  res.cookie('user', json_str, {
+  var IFollowYou = req.cookies.IFollowYou;
+  var test = "".concat(IFollowYou, " =>  ").concat(req.body.buttonInnertext);
+  res.cookie('IFollowYou', json_str, {
     maxAge: 5000000,
     httpOnly: true
   });
   var json_perse = req.cookies.user;
   var arr = JSON.parse(json_perse);
   res.send({
-    ok: true,
-    arr: arr
+    json_str: json_str
   });
-});
+}); // app.post('/buttonsClicked', (req, res) => {
+// let {IFollowYou} = req.cookies;
+//  console.log(IFollowYou);
+// const test = `${IFollowYou} =>  ${req.body.buttonInnertext}`;
+// res.cookie('IFollowYou',test, { maxAge: 5000000, httpOnly: true }); 
+// res.send({
+//     test
+// });
+
 app.use(express["static"]('public'));
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
