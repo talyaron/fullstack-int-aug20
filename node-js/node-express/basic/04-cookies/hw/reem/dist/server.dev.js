61a48652c255d6fee574cac2adaaace4375ceaf8
reem <reemshapiro@gmail.com>
2020-12-15 16:21:29 +0200

    finish hw 04

--- node-js/node-express/basic/04-cookies/hw/reem/dist/server.dev.js
+++ node-js/node-express/basic/04-cookies/hw/reem/dist/server.dev.js
@@ -12,29 +12,24 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 var user = [];
 app.post('/send-user', function (req, res) {
-  user.push(req.body.user);
-  var json_str = JSON.stringify(user); // const {user} = req.body;
-
   var IFollowYou = req.cookies.IFollowYou;
-  var test = "".concat(IFollowYou, " =>  ").concat(req.body.user);
-  res.cookie('IFollowYou', json_str, {
+  console.log(IFollowYou);
+  var test = '';
+
+  if (IFollowYou !== undefined) {
+    test = "".concat(IFollowYou, " =>  ").concat(req.body.buttonInnertext);
+  } else {
+    test = ">> ".concat(req.body.buttonInnertext);
+  }
+
+  res.cookie('IFollowYou', test, {
     maxAge: 5000000,
     httpOnly: true
   });
-  var json_perse = req.cookies.user;
-  var arr = JSON.parse(json_perse);
   res.send({
-    json_str: json_str
-  });
-}); // app.post('/buttonsClicked', (req, res) => {
-// let {IFollowYou} = req.cookies;
-//  console.log(IFollowYou);
-// const test = `${IFollowYou} =>  ${req.body.buttonInnertext}`;
-// res.cookie('IFollowYou',test, { maxAge: 5000000, httpOnly: true }); 
-// res.send({
-//     test
-// });
-
+    test: test
+  }); // console.log(req.body)
+});
 app.use(express["static"]('public'));
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
--- node-js/node-express/basic/04-cookies/hw/reem/public/js.js
+++ node-js/node-express/basic/04-cookies/hw/reem/public/js.js
@@ -1,25 +1,12 @@
-function handleSubmit(e) {
-    e.preventDefault();
-
-    const user = e.target.value;
-
-    let root = document.querySelector('.root');
-  root.innerHTML += `<h1>${user}</h1>`;
+handelclick = (e) => {
+    let buttonInnertext = e.target.value;
+    console.log(buttonInnertext)
     fetch('/send-user', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ user })
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ buttonInnertext })
+    }).then(res => res.json()).then(data => {
+      document.getElementById('root').innerHTML = `<p>${data.test}</p>`;
     })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            let arr = data.arr;
-            console.log(arr)
-            // document.querySelector('root').innerHTML = `<p>${data.json_str    }</p>`;
-            // let root = document.querySelector('root')
-            // root.innerHTML+=`${user}`
-        })
 
-}
+  }
