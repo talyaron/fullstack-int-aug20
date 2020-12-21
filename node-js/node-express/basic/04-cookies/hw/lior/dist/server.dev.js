7a9f85499dc3623e9a7ed4691afcff1f2f921fa4
lior biton <bitonle@gmail.com>
2020-12-21 17:18:38 +0200

    1

--- node-js/node-express/basic/04-cookies/hw/lior/dist/server.dev.js
+++ node-js/node-express/basic/04-cookies/hw/lior/dist/server.dev.js
@@ -12,14 +12,23 @@ var cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
 app.post('/push', function (req, res) {
-  var count = req.body.pushed.count;
+  var count = req.cookies.count;
+  var pbooten = '';
+  console.log(count);
 
-  if (!count) {
-    res.cookie('count', req.body.pushed, {
-      maxAge: 5000000,
-      httpOnly: false
-    });
+  if (count !== undefined) {
+    pbooten = "".concat(count, " -- ").concat(req.body.pushed);
+  } else {
+    pbooten = "".concat(req.body.pushed);
   }
+
+  res.cookie('count', pbooten, {
+    maxAge: 5000000,
+    httpOnly: false
+  });
+  res.send({
+    pbooten: pbooten
+  });
 });
 app.use(express["static"]('public'));
 var port = process.env.PORT || 3000;
