7e91fed30c0e9ebd0d6f39d1267e405bb54281a5
Morde Yoiris <morde1515@gmail.com>
2020-12-24 17:18:55 +0200

    update

--- node-js/node-express/basic/06-CRUD/Morde/public/dist/js.dev.js
+++ node-js/node-express/basic/06-CRUD/Morde/public/dist/js.dev.js
@@ -6,7 +6,7 @@ function handleLoad() {
   }).then(function (data) {
     data.productsArray.forEach(function (product) {
       var root = document.getElementById("root");
-      root.innerHTML += "<div><img class='productPic' src='".concat(product.img, "'/><h2 class='productTitle'>").concat(product.title, "</h2><p class='productPrice'>").concat(product.price, "$</p><form onsubmit=\"handleUpdatePrice(event)\">\n        <input type=\"text\" placeholder=\"Enter A New Price\" name=\"newPrice\" />\n        <div><button class=\"formBtn\" type=\"submit\">Update</button><div>\n      </form></div>");
+      root.innerHTML += "<div><img class='productPic' src='".concat(product.img, "'/><h2 class='productTitle'>").concat(product.title, "</h2><p class='productPrice'>").concat(product.price, "$</p><form onsubmit=\"handleUpdatePrice(event)\">\n        <input type=\"number\" placeholder=\"Enter A New Price\" name=\"newPrice\" />\n        <div><button class=\"formBtn\" type=\"submit\">Update</button><div>\n      </form></div>");
     });
   });
 }
