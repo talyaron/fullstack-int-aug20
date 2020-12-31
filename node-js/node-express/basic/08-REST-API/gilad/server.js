7328324f20db2caa7dc7f5c2e748b00adf665ec1
Gilad <Beer>
2020-12-31 08:03:10 +0200

    1

---
+++ node-js/node-express/basic/08-REST-API/gilad/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express(); ///server;
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { response } = require('express');
+const fetch = require('node-fetch');
+
+app.use(cookieParser());
+app.use(express.static('public'));
+app.use(bodyParser.json())
+
+
+app.post('/sendCity', (req, res) => {
+
+
+   
+    const { city } = req.body;
+    console.log(city)
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9c72c68beca5025e3cc723b7e0045386&units=metric`) //event loop
+        .then(res => res.json())
+        .then(weather => {
+            res.send({ ok: true, weather }) //asynchornic programing
+        });
+
+
+    console.log('cont')
+
+})
+
+
+
+app.listen(3000, () => { console.log('listen 3000') })
