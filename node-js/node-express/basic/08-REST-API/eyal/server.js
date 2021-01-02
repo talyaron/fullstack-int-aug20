72bd7d4d0a79bbc7ab22fb002360f28daebd4e10
eyal home <eshemuel1@gmail.com>
2021-01-02 12:57:49 +0200

    starting face ditecsion

--- node-js/node-express/basic/08-REST-API/eyal/server.js
+++ node-js/node-express/basic/08-REST-API/eyal/server.js
@@ -2,6 +2,14 @@
  *  Author : eyal shemuel
  *  Created On : Mon Dec 28 2020
  *  File : server.js.js
+ * 
+ * https://rapidapi.com/cloud-actions-cloud-actions-default/api/google-ai-vision?endpoint=apiendpoint_9279cd90-1910-4cae-b5ff-c3ea68bc9777
+ * this api can:
+ *         find faces in image
+ *         Detect Image Object
+ *         Facial Emotion Recognition
+ *         covert Image to Text
+ * and more
  *******************************************/
 
 const express = require('express');
@@ -44,7 +52,8 @@ res.send({hebTransaction});
 async function SendMessageToRobot(massage) {
   let robotResponseMsg;
   await fetch(
-    `https://acobot-brainshop-ai-v1.p.rapidapi.com/get?bid=178&key=sX5A2PcYZbsN5EY6&uid=mashape&msg=${massage}`,
+  //  `https://acobot-brainshop-ai-v1.p.rapidapi.com/get?bid=178&key=sX5A2PcYZbsN5EY6&uid=mashape&msg=${massage}`,
+   ` https://acobot-brainshop-ai-v1.p.rapidapi.com/get?bid=154389&key=MqKbUKOqNKyU0Dmg&uid=moshe&msg=${massage}`,
     {
       method: 'GET',
       headers: {
@@ -63,12 +72,30 @@ async function SendMessageToRobot(massage) {
   return robotResponseMsg;
 }
 
+async function setKey(){
+  let key;
+ await fetch("https://translated-mymemory---translation-memory.p.rapidapi.com/createkey", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "2dae7de7a8msh9ca6fa97f167561p1494d2jsn956ba9663ea0",
+		"x-rapidapi-host": "translated-mymemory---translation-memory.p.rapidapi.com"
+	}
+})
+.then(response => {
+	key = response;
+})
+.catch(err => {
+	console.error(err);
+});
+return key;
+}
 
 async function translate(fromLng, toLng, massage) {
   let translatedMessage;
   let textToTranslate =  encode(massage);
-
-  await fetch(`https://translated-mymemory---translation-memory.p.rapidapi.com/api/get?langpair=${fromLng}%7C${toLng}&q=${textToTranslate}&mt=1&onlyprivate=1&de=a%40b.c`, {
+//let Tkey = await setKey;// no nead - so canceld
+  await fetch(`
+  https://translated-mymemory---translation-memory.p.rapidapi.com/api/get?langpair=${fromLng}%7C${toLng}&q=${textToTranslate}&mt=1&onlyprivate=1&de=a%40b.c`,  {
     "method": "GET",
     "headers": {
       "x-rapidapi-key": "2dae7de7a8msh9ca6fa97f167561p1494d2jsn956ba9663ea0",
@@ -84,7 +111,28 @@ async function translate(fromLng, toLng, massage) {
   return translatedMessage;
 }
 
+const getFaceFromeImage =  async (imgeUrl) => {
+//how to get img from webcam   -   https://medium.com/swlh/how-to-access-webcam-and-take-picture-with-javascript-b9116a983d78
+//                                  https://bensonruan.com/how-to-access-webcam-and-take-photo-with-javascript/
+  const form = new FormData(img);
+  form.append("image", `${img}`);
+  
+ await fetch("https://facedetection9.p.rapidapi.com/faces", {
+    "method": "POST",
+    "headers": {
+      "content-type": "multipart/form-data; boundary=---011000010111000001101001",
+      "x-rapidapi-key": "2dae7de7a8msh9ca6fa97f167561p1494d2jsn956ba9663ea0",
+      "x-rapidapi-host": "facedetection9.p.rapidapi.com"
+    }
+  })
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.error(err);
+  });
 
+}
 
 app.listen(port, () => {
   console.log(`Listen on port ${port}`);
