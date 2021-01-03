7a96edf2d0a9aaf52cdb24566bbcf186e837888f
cherut gur <cherut.gur@gmail.com>
2021-01-03 22:14:33 +0200

    11

--- node-js/node-express/basic/09-Promises/HW/cherut/public/script.js
+++ node-js/node-express/basic/09-Promises/HW/cherut/public/script.js
@@ -14,6 +14,7 @@ async function getData() {
 
 }
 
+
 getData()
 
 async function getImg() {
@@ -51,5 +52,28 @@ async function getLyrics() {
     .catch(e => {
       console.error(e);
     })
-    return status;
+  return status;
 }
+
+
+// xmlh
+const data = null;
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === this.DONE) {
+    console.log(this.responseText);
+  }
+});
+
+xhr.open("GET", "https://sridurgayadav-chart-lyrics-v1.p.rapidapi.com/apiv1.asmx/SearchLyricDirect?artist=michael%20jackson&song=bad");
+xhr.setRequestHeader("x-rapidapi-key", "3141833d07msh1c4d90f6d62efb2p1b4fccjsn887bca0704a2");
+xhr.setRequestHeader("x-rapidapi-host", "sridurgayadav-chart-lyrics-v1.p.rapidapi.com");
+
+xhr.send(data);
+// xmlh
+
+
+
