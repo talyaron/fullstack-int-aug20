8d35155bf4d7d819d28b8d1c542bc6f3005582c4
shneur <770abhtur1@gmail.com>
2021-01-10 17:17:08 +0200

    dfghjk

--- node-js/node-express/basic/08-REST-API/example/shneur/server.js
+++ node-js/node-express/basic/08-REST-API/example/shneur/server.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express(); ///server;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const { response } = require('express');
+const {
+    response
+} = require('express');
 const fetch = require('node-fetch');
 
 app.use(cookieParser());
@@ -11,29 +13,38 @@ app.use(bodyParser.json())
 
 
 app.post('/sendStock', (req, res) => {
+    try {
+        const {
+            myStock
+        } = req.body;
+        console.log(myStock)
 
 
-   
-    const { myStock } = req.body;
-    console.log(myStock)
+        fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete?q=tesla&region=US", {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-key": "e846b0c7f4msha2c3878c64cee80p17095djsn12b3512845a6",
+                    "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
+                }
+            })
+            .then(response => {
+                console.log(response);
+                res.send(response)
+            })
+            .catch(err => {
+                console.error(err);
+            });
 
-    fetch("https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums?album_type=SINGLE&offset=20&limit=10", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "e846b0c7f4msha2c3878c64cee80p17095djsn12b3512845a6",
-            "x-rapidapi-host": "finage-currency-data-feed.p.rapidapi.com"
-        }
-    })
-    .then(response => {
-        console.log(response);
-    })
-    .catch(err => {
-        console.error(err);
-    }); 
-   
+    } catch (e) {
+        console.log(` erorr ${e}`)
+    }
+
+
 
 })
 
 
 
-app.listen(3000, () => { console.log('listen 3000') })
+app.listen(3000, () => {
+    console.log('listen 3000')
+})
