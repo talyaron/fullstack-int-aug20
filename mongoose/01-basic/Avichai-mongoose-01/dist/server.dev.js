ac7107876062e3b32ab69936d3fd63523f77ed77
Avichai Nissim <avichai74@gmail.com>
2021-01-10 12:57:30 +0200

    1

--- mongoose/01-basic/Avichai-mongoose-01/dist/server.dev.js
+++ mongoose/01-basic/Avichai-mongoose-01/dist/server.dev.js
@@ -12,6 +12,9 @@ var url = 'mongodb+srv://avichai:123@cluster0.7lig6.mongodb.net/test';
 var Schema = mongoose.Schema;
 app.use(bodyParser.json());
 app.use(express["static"]('public'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -28,7 +31,8 @@ var bmw = new Car({
   price: 60000
 }); // bmw.save().then(doc => console.log(doc)).catch(e => { console.log(e) });
 
-var Email = mongoose.model('User', {
+var User = mongoose.model('User', {
+  // scheme for adding user/email
   email: {
     type: String,
     unique: true,
@@ -51,23 +55,82 @@ var Email = mongoose.model('User', {
 
   }
 });
-var email = new Email({
-  email: 'avichai74@gmail.com',
+var user = new User({
+  email: 'aftat@gmail.com',
   password: '123',
   username: '123'
-}); // email.save().then(data => console.log(data)).catch(e => { console.log(e) });
+}); // user.save().then(data => console.log(data)).catch(e => { console.log(e) });
 
 app.get('/users', function (req, res) {
   console.log('finding users.');
-  Email.find({}).exec(function (err, info) {
+  User.find({}).exec(function (err, info) {
     if (err) {
       res.send({
         ok: false
       });
     } else {
+      res.json(info);
+    }
+  });
+});
+app.get('/users/:id', function (req, res) {
+  console.log('finding user.');
+  User.findOne({
+    _id: req.params.id
+  }).exec(function (err, user) {
+    if (err) {
       res.send({
-        info: info
+        ok: false
       });
+    } else {
+      console.log(user);
+      res.send(user);
+    }
+  });
+});
+app.post('/users', function (req, res) {
+  var newUser = new User();
+  newUser.email = req.body.email;
+  newUser.username = req.body.username;
+  newUser.password = req.body.password;
+  newUser.save().then(function (user) {
+    return console.log(user);
+  }, res.send(user))["catch"](function (e) {
+    return console.log(e);
+  });
+});
+app.post('/users2', function (req, res) {
+  User.create(req.body).then(function (user) {
+    return console.log(user);
+  }, res.send(user))["catch"](function (e) {
+    return console.log(e);
+  });
+});
+app.put('/user/:id', function (req, res) {
+  User.findOneAndUpdate({
+    _id: req.params.id
+  }, {
+    $set: {
+      email: req.body.email
+    }
+  }, function (err, newUser) {
+    if (err) {
+      console.log('error');
+    } else {
+      console.log(newUser);
+      res.send(newUser);
+    }
+  });
+});
+app["delete"]('/user/:id', function (req, res) {
+  User.findOneAndRemove({
+    _id: req.params.id
+  }, function (err, user) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("deleteing user ".concat(user));
+      res.sendStatus(204);
     }
   });
 });
--- mongoose/01-basic/Avichai-mongoose-01/server.js
+++ mongoose/01-basic/Avichai-mongoose-01/server.js
@@ -7,7 +7,7 @@ const Schema = mongoose.Schema
 
 app.use(bodyParser.json());
 app.use(express.static('public'))
-
+app.use(bodyParser.urlencoded({ extended: true }))
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useNewUrlParser', true);
@@ -22,7 +22,7 @@ const Car = mongoose.model('Car', {
 const bmw = new Car({ name: 'Hundai', price: 60000 });
 // bmw.save().then(doc => console.log(doc)).catch(e => { console.log(e) });
 
-const Email = mongoose.model('User', {
+const User = mongoose.model('User', { // scheme for adding user/email
     email: {
         type: String,
         unique: true, // if email exist in db section, will not be created!
@@ -41,20 +41,73 @@ const Email = mongoose.model('User', {
         default: Date.now // give default date if user does not enter date.
     }
 })
-const email = new Email({ email: 'avichai74@gmail.com', password: '123', username: '123' })
-// email.save().then(data => console.log(data)).catch(e => { console.log(e) });
+const user = new User({ email: 'aftat@gmail.com', password: '123', username: '123' })
+// user.save().then(data => console.log(data)).catch(e => { console.log(e) });
 
 app.get('/users', (req, res) => {
     console.log('finding users.')
-    Email.find({})
+    User.find({})
         .exec(function (err, info) {
             if (err) {
                 res.send({ ok: false })
             } else {
-                res.send({info})
+                res.json(info)
             }
         })
+})
+app.get('/users/:id', (req, res) => {
+    console.log('finding user.')
+    User.findOne({
+        _id: req.params.id
+    }).exec(function (err, user) {
+        if (err) {
+            res.send({ ok: false })
+        } else {
+            console.log(user)
+            res.send(user)
+        }
+    })
+})
+app.post('/users', (req, res) => {
+    const newUser = new User();
+    newUser.email = req.body.email
+    newUser.username = req.body.username
+    newUser.password = req.body.password
 
+    newUser.save().then(user => console.log(user), res.send(user)).catch(e => console.log(e))
+})
+app.post('/users2', (req, res) => {
+    User.create(req.body).then(user => console.log(user), res.send(user)).catch(e => console.log(e))
+})
+
+app.put('/user/:id', (req, res) => {
+    User.findOneAndUpdate({
+        _id: req.params.id
+    }, {
+        $set:
+            { email: req.body.email }
+    },
+        function (err, newUser) {
+            if (err) {
+                console.log('error')
+            } else {
+                console.log(newUser)
+                res.send(newUser);
+            }
+        }
+    )
+})
+app.delete('/user/:id', (req, res) => {
+    User.findOneAndRemove({
+        _id: req.params.id
+    }, function (err, user) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(`deleteing user ${user}`)
+            res.sendStatus(204)
+        }
+    })
 })
 
 const port = process.env.PORT || 3000;
