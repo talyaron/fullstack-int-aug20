3efae142bbb73730dbb76e101a38643288774cf0
hillel <a4105962@gmail.com>
2021-01-11 17:55:17 +0200

    1

--- mongoose/01-basic/hillel/server.js
+++ mongoose/01-basic/hillel/server.js
@@ -1,40 +1,58 @@
 const express = require('express');
 const app = express();
-
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
+const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'))
 
 const mongoose = require('mongoose');
 const url = "mongodb+srv://hillel:Aa25802580@cluster0.z35go.mongodb.net/test";
-
-
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const Users = mongoose.model('Car', {
+const users = mongoose.model('user', {
     name: String,
-    price: Number
-});
+    Family: String,
+    tel: Number
+})
 
-const bmw = new Users({ name: 'hillel', price: 12345 });
-bmw.save().then(doc => console.log(doc)).catch(e => { console.log(e) });
-
-
-// const House = mongoose.model('House', {
-//     address: String,
-//     price: Number
-// });
-
-// const House1 = new House({address:'blabla', price:70000})
-// House1.save().then(doc=> console.log(doc))
-
-app.get('/api/geroup', async (req, res) => {
-    let docs = await Car.find({})
-
-    res.send(docs)
-}) 
+app.get('/get', async (req, res) => {
+    const tt = await getserverapi(users)
+    res.send({ tt })
+})
 
 
-const port = process.env.PORT || 3000;
+app.post('/getserver', async (req, res) => {
+    const { name, Family, tel } = req.body
+
+
+    if (name.length > 0 && Family.length > 0) {
+        const userfini = new users({ name, Family, tel })
+        userfini.save()
+            .then(doc => {
+                console.log(doc)
+            })
+            .catch(e => {
+                console.log('ER:' + e)
+            })
+
+        const tt = await getserverapi(users)
+
+        res.send({ tt })
+    }
+
+    else {
+
+    }
+})
+
+async function getserverapi(users) {
+    try {
+        return await users.find({})
+    }
+    catch (e) {
+        console.log('ER:' + e)
+    }
+}
+
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log('server listen on port ', port))
