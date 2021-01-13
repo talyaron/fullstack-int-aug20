04cddb4bd9262be523976b35c9ec208608a24980
shneur <770abhtur1@gmail.com>
2021-01-13 15:10:22 +0200

    ghjk

--- mongoose/01-basic/hilel-and-shneur/public/js.js
+++ mongoose/01-basic/hilel-and-shneur/public/js.js
@@ -7,33 +7,34 @@ function mysubmit(event) {
     const number = event.target.children.number.value
 
     fetch("/sendItem", {
-        method: "post",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            name,
-            catgory,
-            number
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name,
+                catgory,
+                number
+            })
         })
-    })
         .then(a => a.json())
         .then(data => {
             printData(data.data)
         })
 }
+
 function deleteitem(event) {
     const dataid = event.target.dataset.id
 
     fetch("/deleteItem", {
-        method: "post",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            dataid
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                dataid
+            })
         })
-    })
         .then(a => a.json())
         .then(data => {
             printData(data.data)
@@ -54,14 +55,15 @@ function statusItem(event) {
 
     console.log(dataid)
     fetch("/checkIf", {
-        method: "post",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            checkBox, dataid
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                checkBox,
+                dataid
+            })
         })
-    })
         .then(a => a.json())
         .then(data => {
             printData(data.data)
@@ -85,68 +87,45 @@ function printData(data) {
             if (data[i].number == null) {
                 if (data[i].catgory == 'מוצרי חלב') {
                     item1.innerHTML += `<div class="lists" style='background:burlywood;opacity:0.4;color:grey'><input type="checkbox" checked="true" onclick="statusItem(event)"data-id="${data[i]._id}"><div class="list">${data[i].name}</div><div class="list">1</div><img src="img/icons8-close-window-100.png" onclick="deleteitem(event)" data-id="${data[i]._id}"></div>`
-                }
-                else if (data[i].catgory == 'פירות וירקות') {
+                } else if (data[i].catgory == 'פירות וירקות') {
                     item2.innerHTML += `<div class="lists" style='background:burlywood;opacity:0.4;color:grey'><input type="checkbox" checked="true" onclick="statusItem(event)"data-id="${data[i]._id}"><div class="list">${data[i].name}</div><div class="list">1</div><img src="img/icons8-close-window-100.png" onclick="deleteitem(event)" data-id="${data[i]._id}"></div>`
-                }
-                else if (data[i].catgory == 'חד פעמי') {
+                } else if (data[i].catgory == 'חד פעמי') {
                     item3.innerHTML += `<div class="lists" style='background:burlywood;opacity:0.4;color:grey'><input type="checkbox" checked="true" onclick="statusItem(event)"data-id="${data[i]._id}"><div class="list">${data[i].name}</div><div class="list">1</div><img src="img/icons8-close-window-100.png" onclick="deleteitem(event)" data-id="${data[i]._id}"></div>`
-                }
-                else if (data[i].catgory == 'קפואים') {
+                } else if (data[i].catgory == 'קפואים') {
                     item4.innerHTML += `<div class="lists" style='background:burlywood;opacity:0.4;color:grey'><input type="checkbox" checked="true" onclick="statusItem(event)"data-id="${data[i]._id}"><div class="list">${data[i].name}</div><div class="list">1</div><img src="img/icons8-close-window-100.png" onclick="deleteitem(event)" data-id="${data[i]._id}"></div>`
-                }
-                else {
-                }
-            }
-            else {
+                } else {}
+            } else {
                 if (data[i].catgory == 'מוצרי חלב') {
                     item1.innerHTML += `<div class="lists" style='background:burlywood;opacity:0.4;color:grey'><input type="checkbox" checked="true" onclick="statusItem(event)"data-id="${data[i]._id}"><div class="list">${data[i].name}</div><div class="list">${data[i].number}</div><img src="img/icons8-close-window-100.png" onclick="deleteitem(event)" data-id="${data[i]._id}"></div>`
-                }
-                else if (data[i].catgory == 'פרות וירקות') {
+                } else if (data[i].catgory == 'פרות וירקות') {
                     item2.innerHTML += `<div class="lists" style='background:burlywood;opacity:0.4;color:grey'><input type="checkbox" checked="true" onclick="statusItem(event)"data-id="${data[i]._id}"><div class="list">${data[i].name}</div><div class="list">${data[i].number}</div><img src="img/icons8-close-window-100.png" onclick="deleteitem(event)" data-id="${data[i]._id}"></div>`
-                }
-                else if (data[i].catgory == 'חד פעמי') {
+                } else if (data[i].catgory == 'חד פעמי') {
                     item3.innerHTML += `<div class="lists" style='background:burlywood;opacity:0.4;color:grey'><input type="checkbox" checked="true" onclick="statusItem(event)"data-id="${data[i]._id}"><div class="list">${data[i].name}</div><div class="list">${data[i].number}</div><img src="img/icons8-close-window-100.png" onclick="deleteitem(event)" data-id="${data[i]._id}"></div>`
-                }
-                else if (data[i].catgory == 'קפואים') {
+                } else if (data[i].catgory == 'קפואים') {
                     item4.innerHTML += `<div class="lists" style='background:burlywood;opacity:0.4;color:grey'><input type="checkbox" checked="true" onclick="statusItem(event)"data-id="${data[i]._id}"><div class="list">${data[i].name}</div><div class="list">${data[i].number}</div><img src="img/icons8-close-window-100.png" onclick="deleteitem(event)" data-id="${data[i]._id}"></div>`
-                }
-                else {
-                }
+                } else {}
             }
-        }
-        else {
+        } else {
             if (data[i].number == null) {
                 if (data[i].catgory == 'מוצרי חלב') {
                     item1.innerHTML += `<div class="lists"><input type="checkbox" dataset="fauls" onclick="statusItem(event)"data-id="${data[i]._id}"><div class="list">${data[i].name}</div><div class="list">1</div><img src="img/icons8-close-window-100.png" onclick="deleteitem(event)" data-id="${data[i]._id}"></div>`
-                }
-                else if (data[i].catgory == 'פירות וירקות') {
+                } else if (data[i].catgory == 'פירות וירקות') {
                     item2.innerHTML += `<div class="lists"><input type="checkbox" dataset="fauls" onclick="statusItem(event)"data-id="${data[i]._id}"><div class="list">${data[i].name}</div><div class="list">1</div><img src="img/icons8-close-window-100.png" onclick="deleteitem(event)" data-id="${data[i]._id}"></div>`
-                }
-                else if (data[i].catgory == 'חד פעמי') {
+                } else if (data[i].catgory == 'חד פעמי') {
                     item3.innerHTML += `<div class="lists"><input type="checkbox" dataset="fauls" onclick="statusItem(event)"data-id="${data[i]._id}"><div class="list">${data[i].name}</div><div class="list">1</div><img src="img/icons8-close-window-100.png" onclick="deleteitem(event)" data-id="${data[i]._id}"></div>`
-                }
-                else if (data[i].catgory == 'קפואים') {
+                } else if (data[i].catgory == 'קפואים') {
                     item4.innerHTML += `<div class="lists"><input type="checkbox" dataset="fauls" onclick="statusItem(event)"data-id="${data[i]._id}"><div class="list">${data[i].name}</div><div class="list">1</div><img src="img/icons8-close-window-100.png" onclick="deleteitem(event)" data-id="${data[i]._id}"></div>`
-                }
-                else {
-                }
-            }
-            else {
+                } else {}
+            } else {
                 if (data[i].catgory == 'מוצרי חלב') {
                     item1.innerHTML += `<div class="lists"><input type="checkbox"dataset="fauls" onclick="statusItem(event)"data-id="${data[i]._id}"><div class="list">${data[i].name}</div><div class="list">${data[i].number}</div><img src="img/icons8-close-window-100.png" onclick="deleteitem(event)" data-id="${data[i]._id}"></div>`
-                }
-                else if (data[i].catgory == 'פרות וירקות') {
+                } else if (data[i].catgory == 'פרות וירקות') {
                     item2.innerHTML += `<div class="lists"><input type="checkbox"dataset="fauls" onclick="statusItem(event)"data-id="${data[i]._id}"><div class="list">${data[i].name}</div><div class="list">${data[i].number}</div><img src="img/icons8-close-window-100.png" onclick="deleteitem(event)" data-id="${data[i]._id}"></div>`
-                }
-                else if (data[i].catgory == 'חד פעמי') {
+                } else if (data[i].catgory == 'חד פעמי') {
                     item3.innerHTML += `<div class="lists"><input type="checkbox"dataset="fauls" onclick="statusItem(event)"data-id="${data[i]._id}"><div class="list">${data[i].name}</div><div class="list">${data[i].number}</div><img src="img/icons8-close-window-100.png" onclick="deleteitem(event)" data-id="${data[i]._id}"></div>`
-                }
-                else if (data[i].catgory == 'קפואים') {
+                } else if (data[i].catgory == 'קפואים') {
                     item4.innerHTML += `<div class="lists"><input type="checkbox"dataset="fauls" onclick="statusItem(event)"data-id="${data[i]._id}"><div class="list">${data[i].name}</div><div class="list">${data[i].number}</div><img src="img/icons8-close-window-100.png" onclick="deleteitem(event)" data-id="${data[i]._id}"></div>`
-                }
-                else {
-                }
+                } else {}
             }
         }
     }
