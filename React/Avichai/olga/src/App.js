bc33e8c367b831ab0466354eed2092cc46ffd782
OlgaPan-2020 <72758446+OlgaPan-2020@users.noreply.github.com>
2021-02-15 17:33:55 +0200

    1

---
+++ React/Avichai/olga/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+let x= 42;
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload {x}.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>Hello</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
