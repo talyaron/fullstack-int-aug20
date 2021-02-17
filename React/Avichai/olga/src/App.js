4d01d124ce8d9bb9d7f1a50fd4a3c1e356720f92
OlgaPan-2020 <72758446+OlgaPan-2020@users.noreply.github.com>
2021-02-18 01:58:27 +0200

    randomImg

--- React/Avichai/olga/src/App.js
+++ React/Avichai/olga/src/App.js
@@ -1,28 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
-let x= 42;
+//components
+import Box from './view/components/Box/Box';
+
+
+
+
+
+
 
 function App() {
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload {x}.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Hello</p>
+        
+        
+        
+        <Box/>
+       
       </header>
+
     </div>
+
   );
 }
+
+
 
 export default App;
