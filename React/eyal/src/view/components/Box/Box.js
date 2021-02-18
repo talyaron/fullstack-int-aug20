72bcae4340f74a6e099a9f7716e1efd00e6c20aa
eyal shemuel <eshemuel1@gmail.com>
2021-02-18 19:39:19 +0200

    1

--- React/eyal/src/view/components/Box/Box.js
+++ React/eyal/src/view/components/Box/Box.js
@@ -3,17 +3,26 @@ import React, { useState } from 'react';
 function Box(props) {
   const { carType, speed, d } = props;
   const [random, setRandom] = useState(0);
-  
+  const [url, setUrl] = useState('');
+
   function changePic(){
     let random1 = Math.floor(Math.random() * 4)
     setRandom(random1)
     console.log(random1)
   }
+  function handleSubmit(e){
+    e.preventDefault();
+    e.target.reset();
+}
 
   return (
     <div onClick={changePic} style={{background:"pink"}}>
-      <img  src={d[random].src} className="App-logo" alt="logo"  />
-      <h1 >this is {carType} and her max speed is {speed} mph</h1>
+      <img  src={(e)=>()} className="App-logo" alt="logo"  />
+      <form onSubmit={handleSubmit}>
+      <input type="text" name="input" id="imgInput"/>
+      <input type="button" value="OK"/>
+      </form>
+      
     </div>
   )
 
